d09804d3378dfc43f82901f679beeb9aecc36985
erickkramer <erick.romero@smail.inf.h-brs.de>
2018-06-26 09:25:18 +0200

    Bug in the FNDS and the dom_validator function fixed

--- 04/code/crowding_distance.m
+++ 04/code/crowding_distance.m
@@ -1,4 +1,5 @@
 function individual =  crowding_distance(F, individual, fitness_ones, fitness_zeros)
+
     for fr= 1:size(F,2) 
         
         % Extract front_ind in the front
@@ -10,26 +11,26 @@ function individual =  crowding_distance(F, individual, fitness_ones, fitness_ze
         end
         
         % Sort by fitness
-        [~,I_ones] = sort(fitness_ones(front_ind));
-        [~,I_zeros] = sort(fitness_zeros(front_ind));
+        [sorted_ones,I_ones] = sort(fitness_ones(front_ind));
+        [sorted_zeros,I_zeros] = sort(fitness_zeros(front_ind));
         
         % Assign infinite distance to boundaries
         individual(front_ind(I_ones(1))).distance = inf;
         individual(front_ind(I_ones(end))).distance = inf;
         
         individual(front_ind(I_zeros(1))).distance = inf;
-        individual(front_ind(I_zeros(1))).distance = inf;
+        individual(front_ind(I_zeros(end))).distance = inf;
         
         for k = 2:size(front_ind,1) - 1
-            if max(fitness_ones(front_ind)) - min(fitness_ones(front_ind)) ~= 0
+            if (max(sorted_ones) - min(sorted_ones)) ~= 0
                 individual(I_ones(k)).distance = individual(I_ones(k)).distance + ...
                     (fitness_ones(I_ones(k+1)) - fitness_ones(I_ones(k-1))/...
-                    (max(fitness_ones(front_ind)) - min(fitness_ones(front_ind))));
+                    (max(sorted_ones) - min(sorted_ones)));
             end
-            if max(fitness_zeros(front_ind)) - min(fitness_zeros(front_ind)) ~= 0
+            if (max(sorted_zeros) - min(sorted_zeros)) ~= 0
                 individual(I_zeros(k)).distance = individual(I_zeros(k)).distance + ...
                     (fitness_zeros(I_zeros(k+1)) - fitness_zeros(I_zeros(k-1))/...
-                    (max(fitness_zeros(front_ind)) - min(fitness_zeros(front_ind))));
+                    (max(sorted_zeros) - min(sorted_zeros)));
             end
         end
         
--- 04/code/dom_validator.m
+++ 04/code/dom_validator.m
@@ -1,14 +1,19 @@
 function dominates = dom_validator(ind_A, ind_B, fitness_ones, fitness_zeros)
     % Validate if ind_A dominate ind_B
     
-    if (fitness_ones(ind_A) > fitness_ones(ind_B) && ...
-       fitness_zeros(ind_A) >= fitness_zeros(ind_B) || ...
-       fitness_zeros(ind_A) > fitness_zeros(ind_B) && ...
-       fitness_zeros(ind_A) >= fitness_zeros(ind_B))
-       dominates = true;
-    else
-        dominates = false;
-    end
+%     if (fitness_ones(ind_A) > fitness_ones(ind_B) && ...
+%        fitness_zeros(ind_A) >= fitness_zeros(ind_B) || ...
+%        fitness_zeros(ind_A) > fitness_zeros(ind_B) && ...
+%        fitness_zeros(ind_A) >= fitness_zeros(ind_B))
+%        dominates = true;
+%     else
+%         dominates = false;
+%     end
+    
+    ind_A_fit = [fitness_zeros(ind_A), fitness_ones(ind_A)];
+    ind_B_fit = [fitness_zeros(ind_B), fitness_ones(ind_B)];
+    dominates = (all(ind_A_fit >= ind_B_fit) && ...
+                 any(ind_A_fit > ind_B_fit));
     
    
 end
